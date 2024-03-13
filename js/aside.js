196001124f3e6d51a6a6db9c1509a626faec9ee3
Marlo Dela Torre <marlodelatorre11@gmail.com>
2024-03-13 17:42:45 +0800

    with station cards

---
+++ js/aside.js
@@ -0,0 +1,252 @@
+
+/**
+ * @typedef {{ name: string, image: string, distance: number, price: number, fuel: string, status: 'up' | 'down' }} Station
+ *  
+ */
+
+export class Aside {
+  static MODES = {
+    SEARCH: 'search',
+    RESULT: 'result',
+  }
+
+  static FUEL = {
+    PETROL: 'petrol',
+    DIESEL: 'diesel',
+  }
+
+  static SORT_BY = {
+    PRICE: 'price',
+    DISTANCE: 'distance',
+  }
+
+  state = {
+    mode: Aside.MODES.SEARCH,
+    fuel: Aside.FUEL.PETROL,
+    search: '',
+    sort: Aside.SORT_BY.PRICE,
+
+    /**
+     * @type {Station[]}
+     */
+    items: [],
+  }
+
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+  asideEl = null;
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+
+  searchLabelEl = null;
+
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+  fuelLabelEl = null;
+
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+  sortByEl = null;
+
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+  templateEl = null;
+
+  /**
+   * @type {JQuery<HTMLElement> | null}
+   */
+  listEl = null;
+
+  constructor({
+    container,
+    searchLabel,
+    fuelLabel,
+    sortBy,
+    template,
+    list
+  }) {
+    this.asideEl = container;
+    this.searchLabelEl = searchLabel;
+    this.fuelLabelEl = fuelLabel;
+    this.sortByEl = sortBy;
+    this.templateEl = template;
+    this.listEl = list;
+    this.mode = this.state.mode;
+
+    // this.search('test', 'diesel')
+  }
+
+  get mode() {
+    return this.state.mode;
+  }
+
+  set mode(value) {
+    this.state.mode = value;
+    this.asideEl.attr('mode', value);
+  }
+
+  get sortBy() {
+    return this.state.sort;
+  }
+
+  set sortBy(value) {
+    this.state.sort = value;
+    this.sortByEl.attr('value', value);
+
+    this.generateList();
+  }
+
+  search(search, fuel) {
+    this.state.search = search;
+    this.state.fuel = fuel;
+
+    this.mode = Aside.MODES.RESULT;
+
+    this.searchLabelEl.text(search);
+    this.fuelLabelEl.text(fuel);
+
+    const items = this.getItems(search, fuel);
+    this.items = items;
+
+    this.generateList();
+  }
+
+  generateList() {
+    const items = this.sortItem(this.items);
+
+    this.listEl.html('');
+
+    items.forEach((item) => {
+      const template = this.templateEl.clone(true);
+
+      template.removeClass('template');
+
+      template.attr('status', item.status);
+      template.find('.image').attr('src', item.src).attr('alt', item.name);
+      template.find('.price').text(item.price);
+
+      this.listEl.append(template);
+    });
+  }
+
+  /**
+   * 
+   * @param {Station[]} items 
+   * @returns {Station[]}
+   */
+  sortItem(items) {
+    return items.slice(0).sort((a, b) => {
+      if (this.sortBy === Aside.SORT_BY.PRICE) {
+        return a.price - b.price;
+      }
+
+      return a.distance - b.distance;
+    })
+  }
+
+  getItems() {
+    return [
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 136.9,
+        distance: 100,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 121.0,
+        distance: 120,
+        status: 'down',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 122.2,
+        distance: 110,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 111.3,
+        distance: 90,
+        status: 'down',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      }, {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 111.3,
+        distance: 90,
+        status: 'down',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      }, {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 111.3,
+        distance: 90,
+        status: 'down',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      },
+      {
+        name: 'Tesco',
+        src: '/images/tesco.svg',
+        price: 118.5,
+        distance: 40,
+        status: 'up',
+      }
+    ]
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { Aside } from './aside.js';
+
 $(() => {
 
   function debounce(cb, delay) {
@@ -9,101 +11,21 @@ $(() => {
     }
   }
 
-  class Aside {
-    static MODES = {
-      SEARCH: 'search',
-      RESULT: 'result',
-    }
-
-    static FUEL = {
-      PETROL: 'petrol',
-      DIESEL: 'diesel',
-    }
-
-    static SORT_BY = {
-      PRICE: 'price',
-      DISTANCE: 'distance',
-    }
-
-
-    state = {
-      mode: Aside.MODES.RESULT,
-      fuel: Aside.FUEL.PETROL,
-      search: '',
-      sort: Aside.SORT_BY.PRICE,
-    }
-
-    /**
-     * @type {JQuery<HTMLElement> | null}
-     */
-    asideEl = null;
-    /**
-     * @type {JQuery<HTMLElement> | null}
-     */
-
-    searchLabelEl = null;
-    /**
-     * @type {JQuery<HTMLElement> | null}
-     */
-    fuelLabelEl = null;
-    /**
-     * @type {JQuery<HTMLElement> | null}
-     */
-    sortByEl = null;
-
-    constructor({
-      container,
-      searchLabel,
-      fuelLabel,
-      sortBy,
-    }) {
-      this.asideEl = container;
-      this.searchLabelEl = searchLabel;
-      this.fuelLabelEl = fuelLabel;
-      this.sortByEl = sortBy;
-
-      this.mode = this.state.mode;
-    }
-
-    get mode() {
-      return this.state.mode;
-    }
-
-    set mode(value) {
-      this.state.mode = value;
-      this.asideEl.attr('mode', value);
-    }
-
-    get sortBy() {
-      return this.state.sort;
-    }
-
-    set sortBy(value) {
-      this.state.sort = value;
-      this.sortByEl.attr('value', value);
-    }
-
-    search(search, fuel) {
-      this.state.search = search;
-      this.state.fuel = fuel;
-
-      this.mode = Aside.MODES.RESULT;
-
-      this.searchLabelEl.text(search);
-      this.fuelLabelEl.text(fuel);
-    }
-  }
-
   const $asideEl = $('aside');
   const $searchForm = $asideEl.find('form');
   const $searchBtnIcon = $asideEl.find('.input-group-append')
   const $sortBy = $('.sortBy');
+  const $list = $('.list');
+
+  const $template = $('.station-card.template');
 
   const asideController = new Aside({
     container: $asideEl,
     searchLabel: $asideEl.find('.search-label'),
     fuelLabel: $asideEl.find('.fuel-label'),
     sortBy: $sortBy,
+    template: $template,
+    list: $list,
   });
 
   $searchForm.on('submit', (event) => {
